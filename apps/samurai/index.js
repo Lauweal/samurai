f5f566ce33e39c34ae5126e91ba98783d2ea461f
刘伟 <lemonpaimc@126.com>
2022-04-26 07:19:53 +0800

    feat:

--- apps/samurai/index.js
+++ apps/samurai/index.js
@@ -2,7 +2,7 @@ import 'expo-dev-client';
 
 import { registerRootComponent } from 'expo';
 
-import App from './src/app/App';
+import App from './app/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
