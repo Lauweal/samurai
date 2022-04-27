1cd6c7066e6aa0e8bbaf676adce296a7ff227599
刘伟 <lemonpaimc@126.com>
2022-04-27 14:56:13 +0800

    chore(帐号): chore(ci): ci

--- apps/h5/webpack.config.js
+++ apps/h5/webpack.config.js
@@ -2,8 +2,17 @@ const getWebpackConfig = require('@nrwl/react/plugins/webpack')
 const { merge } = require('webpack-merge');
 
 module.exports = (config, context) => {
-  console.log(context)
+  if(!context.options.filename)   return merge(getWebpackConfig(config), {
+    module: {
+      rules: [
+        { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader
+      ]
+    }
+  })
   return merge(getWebpackConfig(config), {
+    output: {
+      filename: `${context.options.filename}/[name].[hash].js`,
+    },
     module: {
       rules: [
         { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader
